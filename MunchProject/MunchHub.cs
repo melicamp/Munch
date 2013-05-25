dc53f801aafbaa3f79af39e9545a07087af0c2a5
Luke Skywalker <dziad.polny@gmail.com>
2013-05-25 21:23:28 +0200

    Include SignalR

---
+++ MunchProject/MunchHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace MunchProject
+{
+    public class MunchHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
